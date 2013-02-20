85736d8214cd1e40356fc25a8d6d4ca9bbd21c90
briangallagher <briangal@gmail.com>
2013-02-20 16:46:21 +0000

    first commit

---
+++ cloud/main.js
@@ -0,0 +1,10 @@
+var util = require('util');
+
+exports.myFunction = function(params, callback) {
+  var num = params.num;
+  if(num > 10){
+    return callback({err: 'Num too large'}, {result: num});
+  }
+  callback(undefined, {result: num});
+};
+
