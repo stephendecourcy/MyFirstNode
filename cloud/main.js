b558c65b02573a81aedfe57ae270e16d0ea84edd
stephen.decourcy@gmail.com <stephen.decourcy@gmail.com>
2013-02-26 12:07:58 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 exports.myFunction = function(params, callback) {
   var num = params.num;
-  if(num < 10){
+  if(num > 10){
     return callback({err: 'Num too large'}, {result: num});
   }
   callback(undefined, {result: num});
